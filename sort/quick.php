bbef2ba966beb466f798cf80ddee5a55ae1e5c57
Dzmitry Marozau1 <Dzmitry_Marozau1@epam.com>
2018-09-26 13:41:47 +0300

    Quick sort

--- sort/quick.php
+++ sort/quick.php
@@ -5,13 +5,19 @@ function quickSort(array $array):array
   $length = count($array);
 
   if ($length > 1) {
-    $middleKey = (int)$length / 2;
+    $middleKey = (int)($length / 2);
     $flag = $length % 2;
     for ($leftSideKey = 0; $leftSideKey < $middleKey; $leftSideKey ++) {
       $rightSideKey = ($middleKey * 2) - $leftSideKey;
       if (!$flag) {
         $rightSideKey --;
       }
+
+      var_dump([
+        '$array' => $array,
+        '$leftSideKey' => $leftSideKey,
+        '$rightSideKey' => $rightSideKey,
+      ]);
       if ($array[$leftSideKey] > $array[$rightSideKey]) {
         $bufferValue = $array[$leftSideKey];
         $array[$leftSideKey] = $array[$rightSideKey];
@@ -20,7 +26,22 @@ function quickSort(array $array):array
     }
 
     if ($length > 2) {
+      $leftArray = [];
+      $rightArray = [];
+        for ($v = 0; $v < $middleKey; $v ++) {
+          $leftArray[] = $array[$v];
+        }
+        for ($v = $middleKey; $v < $length; $v ++) {
+          $rightArray[] = $array[$v];
+        }
+        $_leftArray = quickSort($leftArray);
+        $_rightArray = quickSort($rightArray);
 
+        $lengthRightArray = count($rightArray);
+        for ($iRight = 0; $iRight < $lengthRightArray; $iRight ++) {
+          $_leftArray[] = $rightArray[$iRight];
+        }
+        $array = $_leftArray;
     }
   }
 
